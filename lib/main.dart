81d77dc6d1173a1bd4558e51d49a9928e1a8fec8
Zach Bochanski <ztbochanski@gmail.com>
2023-02-23 18:39:21 -0800

    add device orientation preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
   runApp(const App());
 }
