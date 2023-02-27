a41a7ac75bb508bddf6acfa80dcbf7ba6ce14c75
Zach Bochanski <ztbochanski@gmail.com>
2023-02-27 13:31:46 -0800

    add themeMode to app

--- lib/app.dart
+++ lib/app.dart
@@ -6,7 +6,9 @@ const String appTitle = 'Journal';
 const String welcomeMessage = 'Welcome! To start writing, tap the + button';
 
 class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
+  final bool theme;
+
+  const App({Key? key, required this.theme}) : super(key: key);
 
   @override
   AppState createState() => AppState();
@@ -21,11 +23,12 @@ class AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Journal',
+      title: appTitle,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
       darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: widget.theme ? ThemeMode.dark : ThemeMode.light,
       routes: routes,
     );
   }
