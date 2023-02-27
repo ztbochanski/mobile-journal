2452261e594bd549b5f1db437994ac0751feac3b
Zach Bochanski <ztbochanski@gmail.com>
2023-02-27 10:36:50 -0800

    add journal entry model according to design spec

---
+++ lib/models/journal_entry.dart
@@ -0,0 +1,19 @@
+class JournalEntry {
+  final int id;
+  final int rating;
+  final String title;
+  final String body;
+  final DateTime date;
+
+  JournalEntry({
+    required this.id,
+    required this.rating,
+    required this.title,
+    required this.body,
+    required this.date,
+  });
+
+  String toString() {
+    return 'JournalEntry(id: $id, rating: $rating, title: $title, body: $body, date: $date)';
+  }
+}
