6d51c810d9b06d5c1dfd72a89a1f55c5bf90afc5
Zach Bochanski <ztbochanski@gmail.com>
2023-02-28 14:10:52 -0800

    add multi provider and change notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:journal/provider/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 
 import 'app.dart';
@@ -11,5 +13,7 @@ void main() {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
-  runApp(const App());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => ThemeProvider()),
+  ], child: const App()));
 }
