b30ef3b04d3e2cc2eec3029976f07bbab818ee54
Zach Bochanski <ztbochanski@gmail.com>
2023-03-01 23:58:28 -0800

    refactor to include validation more detailed error handling
    pull out building the star logic from constructor

--- lib/widgets/rating_form_field.dart
+++ lib/widgets/rating_form_field.dart
@@ -1,32 +1,56 @@
 import 'package:flutter/material.dart';
 
 class RatingFormField extends FormField<int> {
+  static const int numberOfStars = 5;
   RatingFormField({
     Key? key,
     FormFieldSetter<int>? onSaved,
     FormFieldValidator<int>? validator,
-    int? initialValue = 0,
+    InputDecoration? decoration,
+    int? initialValue,
   }) : super(
           key: key,
-          initialValue: initialValue,
+          initialValue: initialValue ?? 0,
           onSaved: onSaved,
-          validator: validator,
+          validator: (value) {
+            if (value == null || value < 1 || value > 5) {
+              return 'Please choose a rating';
+            }
+            return validator?.call(value);
+          },
           builder: (FormFieldState<int> state) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                5,
-                (index) => InkWell(
-                  onTap: () {
-                    state.didChange(index + 1);
-                  },
-                  child: Icon(
-                    index < state.value! ? Icons.star : Icons.star_border,
-                    color: Colors.amber,
-                  ),
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: List.generate(
+                      numberOfStars, (index) => _buildStar(index + 1, state)),
                 ),
-              ),
+                if (state.hasError)
+                  Text(
+                    state.errorText!,
+                    style: TextStyle(
+                        color: Theme.of(state.context).colorScheme.error),
+                  ),
+              ],
             );
           },
         );
+
+  static const double _starSize = 48;
+
+  static Widget _buildStar(int value, FormFieldState<int> state) {
+    final icon = Icon(
+      value <= state.value! ? Icons.star : Icons.star_border,
+      size: _starSize,
+      color: Theme.of(state.context).primaryColor,
+    );
+    return GestureDetector(
+      onTap: () {
+        state.didChange(value);
+      },
+      child: icon,
+    );
+  }
 }
