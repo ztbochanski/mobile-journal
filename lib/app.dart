3f6077d94205008528f2ec6cde0245f120932f1c
Zach Bochanski <ztbochanski@gmail.com>
2023-03-02 00:26:00 -0800

    add entry screen route to routes property

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:journal/screens/journal_entry_screen.dart';
 import 'package:journal/screens/new_entry_screen.dart';
 
 import 'package:journal/screens/journal_entries_list_screen.dart';
@@ -15,6 +16,7 @@ class _AppState extends State<App> {
     JournalEntriesListScreen.routeName: (context) =>
         const JournalEntriesListScreen(),
     NewEntryScreen.routeName: (context) => const NewEntryScreen(),
+    JournalEntryScreen.routeName: (context) => const JournalEntryScreen(),
   };
 
   @override
