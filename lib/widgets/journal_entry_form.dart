711df96125f1f107baf31e75cf23db8275067864
Zach Bochanski <ztbochanski@gmail.com>
2023-03-01 22:28:23 -0800

    add journal entry form with 3 widgets to control input
    title widget
    body widget
    rating widget (custom)

---
+++ lib/widgets/journal_entry_form.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import 'package:journal/db/database_manager.dart';
+import 'package:journal/models/journal_entry_dto.dart';
+import 'package:journal/widgets/rating_form_field.dart';
+
+class JournalEntryForm extends StatefulWidget {
+  const JournalEntryForm({Key? key}) : super(key: key);
+
+  @override
+  State<JournalEntryForm> createState() => _JournalEntryFormState();
+}
+
+class _JournalEntryFormState extends State<JournalEntryForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _bodyController = TextEditingController();
+
+  final journalEntryDTO = JournalEntryDTO();
+
+  final databaseManager = DatabaseManager.getInstance();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _bodyController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(10.0), child: titleFormField()),
+            Padding(
+                padding: const EdgeInsets.all(10.0), child: bodyFormField()),
+            Padding(
+                padding: const EdgeInsets.all(10.0), child: ratingFormField()),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  databaseManager.insertJournalEntry(journalEntryDTO);
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget titleFormField() {
+    return TextFormField(
+      controller: _titleController,
+      decoration: const InputDecoration(
+        labelText: 'Title',
+        border: OutlineInputBorder(),
+      ),
+      onSaved: (value) {
+        journalEntryDTO.title = value!;
+      },
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a title';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget bodyFormField() {
+    return TextFormField(
+      controller: _bodyController,
+      decoration: const InputDecoration(
+        labelText: 'Body',
+        border: OutlineInputBorder(),
+      ),
+      keyboardType: TextInputType.multiline,
+      minLines: 2,
+      maxLines: 8,
+      onSaved: (value) {
+        journalEntryDTO.body = value!;
+      },
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a body';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget ratingFormField() {
+    return RatingFormField(
+      initialValue: 0,
+      validator: (value) {
+        if (value == 0) {
+          return 'Please select a rating.';
+        }
+        return null;
+      },
+      onSaved: (value) {
+        journalEntryDTO.rating = value!;
+      },
+    );
+  }
+}
